0e5e2960258b933b4590c9efbd06b7a553e946e7
anissakn <113972167+anissakn@users.noreply.github.com>
2024-09-26 15:34:15 +0200

    Update dall-e 3.py

--- pages/dall-e 3.py
+++ pages/dall-e 3.py
@@ -3,6 +3,9 @@ import streamlit as st
 st.title("Dall-e 3")
 
 # Champs de saisie
-user_input = st.text_input("Donnez votre nom et prénom :")
+user_input = st.text_input("Veuillez entrer une description de l'image que vous souhaitez générer :")
 
 st.write(user_input)
+
+# sidebar
+st.sidebar.text_input("Veuillez entrer la clé OpenIA")
