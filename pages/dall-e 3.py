456034881e929aef7f632803550a7d567fe104e5
anissakn <113972167+anissakn@users.noreply.github.com>
2024-09-26 15:39:52 +0200

    Update dall-e 3.py

--- pages/dall-e 3.py
+++ pages/dall-e 3.py
@@ -9,3 +9,23 @@ st.write(user_input)
 
 # sidebar
 st.sidebar.text_input("Veuillez entrer la clé OpenIA")
+
+# OpenAI pour générer des images
+from openai import OpenAI
+
+
+client = OpenAI(api_key=sk-6uxlenvrwsRO0NlIgqhrg2B-avWNwVlPV8MgbU-QSHT3BlbkFJSc_Lmu6ZS06yDsSmP4kvVEd42Ds95-CXp726oavJMA)
+
+
+
+
+image = client.images.generate(
+    model="dall-e-2",
+    prompt=prompt,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+
+image_url = image.data[0].url
+print(image_url)
