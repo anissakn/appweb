f357eac656dab66b162fa3c443a86e5fe3636456
anissakn <113972167+anissakn@users.noreply.github.com>
2024-09-26 14:28:14 +0200

    Update home.py

--- home.py
+++ home.py
@@ -13,3 +13,6 @@ st.write(user_input)
 
 # Image
 st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbMZFucLMvnC70v26VRtwnsv73EX8FpKxCqA&s")
+
+#sidebar
+st.sidebar.title("Anissa Kone")
