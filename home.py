2141f95c7497c4cec79f47bd6efec80908eae2c6
anissakn <113972167+anissakn@users.noreply.github.com>
2024-09-26 14:30:38 +0200

    Update home.py

--- home.py
+++ home.py
@@ -14,5 +14,8 @@ st.write(user_input)
 # Image
 st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbMZFucLMvnC70v26VRtwnsv73EX8FpKxCqA&s")
 
-#sidebar
+# sidebar
 st.sidebar.title("Anissa Kone")
+
+# vidéo dans la sidebar
+st.sidebar.video("https://www.youtube.com/watch?v=SALF5tOCHLo")
